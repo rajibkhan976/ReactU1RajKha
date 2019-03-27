c71b140bf97e24aa7257082a49427e91c533a5e1
unknown <rajibkhan976@gmail.com>
2019-03-27 14:14:54 +0100

    uppgift 1 React EC Utbildning, March 2019

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div class="container">
+      <div class="row">
+        <div class="col-12 bg-warning">
+          <h2>ReactU3RajKha</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 bg-info">
+          <h3>Dashboard</h3>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
